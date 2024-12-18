58415dfb8845bc5a6d746dea6f9340dffc187a8a
reydan46 <4998320+Reydan46@users.noreply.github.com>
2024-12-18 10:20:37 +0300

    Update compression support and documentation - Added support for decompression using built-in libraries lzma and bz2. - Updated usage of local tools xzcat and bzcat. - Updated README.md with more detailed usage instructions. - Changed the version constraint of the protobuf library in requirements.txt.

--- README.md
+++ README.md
@@ -1,33 +1,82 @@
 # extract_android_ota_payload.py
 
-Extract Android firmware images from an OTA payload.bin file.
+This utility extracts Android firmware images from an OTA `payload.bin` file. With the A/B system update method, the OTA file format has been revised, and this tool enables the extraction and decompression of firmware images packed using the 'brillo' toolset. Incremental firmware images (such as those using source_copy or source_bsdiff operations) are not supported.
 
-With the introduction of the A/B system update, the OTA file format changed.
-This tool allows to extract and decompress the firmware images packed using the 'brillo' toolset.
-
-Incremental firmware images are not supported (source_copy, source_bsdiff operations).
+---
 
 ## Usage
 
-```
-$ extract_android_ota_payload.py <payload.bin> [target_dir]
-  <payload.bin> : file extracted from the OTA zip file or the OTA zip file
-  <target_dir>  : output directory for the extracted file
+```shell
+$ extract_android_ota_payload.py <payload_file> [target_dir] [--use-local-tools]
+  <payload_file>      : Path to the OTA package (either .zip or .bin format)
+  [target_dir]        : Directory to save extracted files (default: '.\output')
+  [--use-local-tools] : Utilize local `xzcat` and `bzcat` tools if available
 ```
 
-## Example
+### Example
+
+To run the utility, ensure your environment is set up with the required Python dependencies:
+
+```shell
+(venv) $ extract_android_ota_payload.py q3_50473320109900510.zip output --use-local-tools
+```
+
+Expected output:
 
 ```
-$ python3 extract_android_ota_payload.py marlin-ota-opm4.171019.021.d1-fd6998a5.zip /tmp/
+Opening ZIP file q3_50473320109900510.zip to extract to C:\Projects\Python\extract_android_ota_payload\output...
+Extracting to target directory: C:\Projects\Python\extract_android_ota_payload\output
 Extracting 'boot.img'
+[==================================================] 48/48
 Extracting 'system.img'
-Extracting 'vendor.img'
+[==================================================] 414/414 
 ...
-Extracting 'modem.img'
+Extracting 'vendor.img'
+[==================================================] 216/216
 ```
+
+---
+
+## Precompiled Version for Windows
+
+For Windows users, a precompiled version of this utility is available in the releases section. This version does not require Python to be installed on your system. Simply download and run the executable file.
+
+---
+
+## Using a Virtual Environment
+
+To manage dependencies effectively, use a Python virtual environment. Here’s how:
+
+1. Create a virtual environment:
+   ```shell
+   py -m venv venv
+   ```
+
+2. Activate the virtual environment:
+   ```shell
+   venv\Scripts\activate.bat
+   ```
+
+3. Install dependencies:
+   ```shell
+   pip install -r requirements.txt
+   ```
+
+4. Run the utility:
+   ```shell
+   extract_android_ota_payload.py <payload_file> [target_dir] [--use-local-tools]
+   ```
+
+---
 
 ## Dependencies
 
+- `protobuf==3.20.3` (strict version required)
+
+Install by using the `requirements.txt` file:
+```shell
+pip install -r requirements.txt
 ```
-python-protobuf,bzcat,xzcat
-```
+
+For local tools option (--use-local-tools):
+- Make sure `xzcat` and `bzcat` are installed on your system.
--- extract_android_ota_payload.py
+++ extract_android_ota_payload.py
@@ -2,143 +2,234 @@
 
 import hashlib
 import os
-import os.path
-import shutil
 import struct
 import subprocess
-import sys
-import zipfile
-
-# from https://android.googlesource.com/platform/system/update_engine/+/refs/heads/master/scripts/update_payload/
+from pathlib import Path
+from zipfile import ZipFile
 import update_metadata_pb2
+from google.protobuf import message
+import argparse
+import shutil
+import sys
+import signal
+import lzma
+import bz2
 
-PROGRAMS = [ 'bzcat', 'xzcat' ]
-
+BLOCK_SIZE = 4096
 BRILLO_MAJOR_PAYLOAD_VERSION = 2
 
+
 class PayloadError(Exception):
-  pass
+    pass
 
-class Payload(object):
-  class _PayloadHeader(object):
-    _MAGIC = b'CrAU'
 
-    def __init__(self):
-      self.version = None
-      self.manifest_len = None
-      self.metadata_signature_len = None
-      self.size = None
+class Payload:
+    class _PayloadHeader:
+        _MAGIC = b'CrAU'
 
-    def ReadFromPayload(self, payload_file):
-      magic = payload_file.read(4)
-      if magic != self._MAGIC:
-        raise PayloadError('Invalid payload magic: %s' % magic)
-      self.version = struct.unpack('>Q', payload_file.read(8))[0]
-      self.manifest_len = struct.unpack('>Q', payload_file.read(8))[0]
-      self.size = 20
-      self.metadata_signature_len = 0
-      if self.version != BRILLO_MAJOR_PAYLOAD_VERSION:
-        raise PayloadError('Unsupported payload version (%d)' % self.version)
-      self.size += 4
-      self.metadata_signature_len = struct.unpack('>I', payload_file.read(4))[0]
+        def __init__(self) -> None:
+            self.version = None
+            self.manifest_len = None
+            self.metadata_signature_len = None
+            self.size = None
 
-  def __init__(self, payload_file):
-    self.payload_file = payload_file
-    self.header = None
-    self.manifest = None
-    self.data_offset = None
-    self.metadata_signature = None
-    self.metadata_size = None
+        def read_from_payload(self, payload_file) -> None:
+            magic = payload_file.read(4)
+            if magic != self._MAGIC:
+                raise PayloadError(f'Invalid payload magic: {magic}')
+            self.version = struct.unpack('>Q', payload_file.read(8))[0]
+            self.manifest_len = struct.unpack('>Q', payload_file.read(8))[0]
+            self.size = 20
+            self.metadata_signature_len = 0
+            if self.version != BRILLO_MAJOR_PAYLOAD_VERSION:
+                raise PayloadError(f'Unsupported payload version ({self.version})')
+            self.size += 4
+            self.metadata_signature_len = struct.unpack('>I', payload_file.read(4))[0]
 
-  def _ReadManifest(self):
-    return self.payload_file.read(self.header.manifest_len)
+    def __init__(self, payload_file) -> None:
+        self.payload_file = payload_file
+        self.header = None
+        self.manifest = None
+        self.data_offset = None
+        self.metadata_signature = None
+        self.metadata_size = None
 
-  def _ReadMetadataSignature(self):
-    self.payload_file.seek(self.header.size + self.header.manifest_len)
-    return self.payload_file.read(self.header.metadata_signature_len);
+    def _read_manifest(self) -> bytes:
+        return self.payload_file.read(self.header.manifest_len)
 
-  def ReadDataBlob(self, offset, length):
-    self.payload_file.seek(self.data_offset + offset)
-    return self.payload_file.read(length)
+    def _read_metadata_signature(self) -> bytes:
+        self.payload_file.seek(self.header.size + self.header.manifest_len)
+        return self.payload_file.read(self.header.metadata_signature_len)
 
-  def Init(self):
-    self.header = self._PayloadHeader()
-    self.header.ReadFromPayload(self.payload_file)
-    manifest_raw = self._ReadManifest()
-    self.manifest = update_metadata_pb2.DeltaArchiveManifest()
-    self.manifest.ParseFromString(manifest_raw)
-    metadata_signature_raw = self._ReadMetadataSignature()
-    if metadata_signature_raw:
-      self.metadata_signature = update_metadata_pb2.Signatures()
-      self.metadata_signature.ParseFromString(metadata_signature_raw)
-    self.metadata_size = self.header.size + self.header.manifest_len
-    self.data_offset = self.metadata_size + self.header.metadata_signature_len
+    def read_data_blob(self, offset: int, length: int) -> bytes:
+        self.payload_file.seek(self.data_offset + offset)
+        return self.payload_file.read(length)
 
-def decompress_payload(command, data, size, hash):
-  p = subprocess.Popen([command, '-'], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-  r = p.communicate(data)[0]
-  if len(r) != size:
-    print("Unexpected size %d %d" % (len(r), size))
-  elif hashlib.sha256(data).digest() != hash:
-    print("Hash mismatch")
-  return r
+    def init(self) -> None:
+        self.header = self._PayloadHeader()
+        self.header.read_from_payload(self.payload_file)
+        manifest_raw = self._read_manifest()
+        self.manifest = update_metadata_pb2.DeltaArchiveManifest()
+        try:
+            self.manifest.ParseFromString(manifest_raw)
+        except message.DecodeError as parse_error:
+            print(f'[WARN] Cannot deserialize Protobuf. Reason: {parse_error}')
+        metadata_signature_raw = self._read_metadata_signature()
+        if metadata_signature_raw:
+            self.metadata_signature = update_metadata_pb2.Signatures()
+            self.metadata_signature.ParseFromString(metadata_signature_raw)
+        self.metadata_size = self.header.size + self.header.manifest_len
+        self.data_offset = self.metadata_size + self.header.metadata_signature_len
 
-def parse_payload(payload_f, partition, out_f):
-  BLOCK_SIZE = 4096
-  for operation in partition.operations:
-    e = operation.dst_extents[0]
-    data = payload_f.ReadDataBlob(operation.data_offset, operation.data_length)
-    out_f.seek(e.start_block * BLOCK_SIZE)
-    if operation.type == update_metadata_pb2.InstallOperation.REPLACE:
-      out_f.write(data)
-    elif operation.type == update_metadata_pb2.InstallOperation.REPLACE_XZ:
-      r = decompress_payload('xzcat', data, e.num_blocks * BLOCK_SIZE, operation.data_sha256_hash)
-      out_f.write(r)
-    elif operation.type == update_metadata_pb2.InstallOperation.REPLACE_BZ:
-      r = decompress_payload('bzcat', data, e.num_blocks * BLOCK_SIZE, operation.data_sha256_hash)
-      out_f.write(r)
-    elif operation.type == update_metadata_pb2.InstallOperation.ZERO:
-      out_f.write(b'\x00' * (e.num_blocks * BLOCK_SIZE))
-    else:
-      raise PayloadError('Unhandled operation type ({} - {})'.format(operation.type,
-                         update_metadata_pb2.InstallOperation.Type.Name(operation.type)))
 
-def main(filename, output_dir):
-  if filename.endswith('.zip'):
-    print("Extracting 'payload.bin' from OTA file...")
-    ota_zf = zipfile.ZipFile(filename)
-    payload_file = open(ota_zf.extract('payload.bin', output_dir), 'rb')
-  else:
-    payload_file = open(filename, 'rb')
+def check_local_tools() -> None:
+    missing_tools = []
+    if not shutil.which("xzcat"):
+        missing_tools.append("xzcat")
+    if not shutil.which("bzcat"):
+        missing_tools.append("bzcat")
 
-  payload = Payload(payload_file)
-  payload.Init()
+    if missing_tools:
+        raise PayloadError(
+            f"Missing required local tools: {', '.join(missing_tools)}. Please install them or do not use --use-local-tools.")
 
-  for p in payload.manifest.partitions:
-    name = p.partition_name + '.img'
-    print("Extracting '%s'" % name)
-    fname = os.path.join(output_dir, name)
-    out_f = open(fname, 'wb')
+
+def decompress_payload(command: str, data: bytes, size: int, expected_hash: bytes) -> bytes:
+    p = subprocess.Popen([command, '-'], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    r = p.communicate(data)[0]
+    validate_decompression(r, data, size, expected_hash)
+    return r
+
+
+def decompress_payload_fallback(compression_type: str, data: bytes, size: int, expected_hash: bytes) -> bytes:
+    result = None
     try:
-      parse_payload(payload, p, out_f)
-    except PayloadError as e:
-      print('Failed: %s' % e)
-      out_f.close()
-      os.unlink(fname)
+        if compression_type == 'xz':
+            result = lzma.decompress(data)
+        elif compression_type == 'bz2':
+            result = bz2.decompress(data)
+        validate_decompression(result, data, size, expected_hash)
+    except (lzma.LZMAError, IOError) as err_decompress:
+        print(f"{compression_type.upper()} decompression failed: {err_decompress}")
+    return result
+
+
+def validate_decompression(result: bytes, data: bytes, expected_size: int, expected_hash: bytes) -> None:
+    if len(result) != expected_size:
+        print(f"Unexpected size {len(result)} {expected_size}")
+    elif hashlib.sha256(data).digest() != expected_hash:
+        print("Hash mismatch")
+
+
+def display_progress(current: int, total: int) -> None:
+    progress = int((current / total) * 50)
+    bar = '=' * progress + '-' * (50 - progress)
+    sys.stdout.write(f'\r[{bar}] {current}/{total}')
+    sys.stdout.flush()
+
+
+def parse_payload(payload_f: Payload, partition, out_f, use_local_tools: bool) -> None:
+    total_operations = len(partition.operations)
+
+    for index, operation in enumerate(partition.operations):
+        extent = operation.dst_extents[0]
+        data = payload_f.read_data_blob(operation.data_offset, operation.data_length)
+        out_f.seek(extent.start_block * BLOCK_SIZE)
+        operation_type = operation.type
+
+        decompression_function = (
+            decompress_payload
+            if use_local_tools else
+            decompress_payload_fallback
+        )
+
+        if operation_type == update_metadata_pb2.InstallOperation.REPLACE:
+            out_f.write(data)
+        elif operation_type in [update_metadata_pb2.InstallOperation.REPLACE_XZ,
+                                update_metadata_pb2.InstallOperation.REPLACE_BZ]:
+            comp_type = 'xz' if operation_type == update_metadata_pb2.InstallOperation.REPLACE_XZ else 'bz2'
+            cmd = 'xzcat' if comp_type == 'xz' else 'bzcat'
+            decompress_function = lambda d, s, h: decompression_function(cmd, d, s,
+                                                                         h) if use_local_tools else decompression_function(
+                comp_type, d, s, h)
+            r = decompress_function(data, extent.num_blocks * BLOCK_SIZE, operation.data_sha256_hash)
+            if r:
+                out_f.write(r)
+        elif operation_type == update_metadata_pb2.InstallOperation.ZERO:
+            out_f.write(b'\x00' * (extent.num_blocks * BLOCK_SIZE))
+        else:
+            raise PayloadError(
+                f'Unhandled operation type ({operation_type} - {update_metadata_pb2.InstallOperation.Type.Name(operation_type)})')
+
+        display_progress(index + 1, total_operations)
+    print()
+
+
+def extract_android_ota_payload(filename: Path, output_dir: Path, use_local_tools: bool) -> None:
+    output_dir = output_dir.resolve()
+    if output_dir != Path.cwd() and output_dir.exists() and any(output_dir.iterdir()):
+        response = input(
+            f"Target directory '{output_dir}' already exists and is not empty. Overwrite contents? (Y/n): ").strip().lower()
+        if response not in ('y', ''):
+            print("Operation cancelled by user.")
+            sys.exit(0)
+
+    if filename.suffix == '.zip':
+        print(f"Opening ZIP file {filename} to extract to {output_dir}...")
+        with ZipFile(filename) as ota_zf:
+            payload_file_path = ota_zf.extract('payload.bin', output_dir)
+            payload_file = open(payload_file_path, 'rb')
+    else:
+        print(f"Opening file {filename} for extraction to {output_dir}...")
+        payload_file = open(filename, 'rb')
+
+    print(f"Extracting to target directory: {output_dir}")
+
+    payload = Payload(payload_file)
+    payload.init()
+
+    for p in payload.manifest.partitions:
+        name = f'{p.partition_name}.img'
+        print(f"Extracting '{name}'")
+        fname = output_dir / name
+        with open(fname, 'wb') as out_f:
+            try:
+                parse_payload(payload, p, out_f, use_local_tools)
+            except PayloadError as err_extract:
+                print(f'Error: {err_extract}')
+                out_f.close()
+                os.unlink(fname)
+                if use_local_tools:
+                    sys.exit(1)
+
+
+# noinspection PyUnusedLocal
+def signal_handler(signal_received, frame) -> None:
+    print("\nOperation cancelled by user.")
+    sys.exit(0)
+
 
 if __name__ == '__main__':
-  try:
-    filename = sys.argv[1]
-  except:
-    print('Usage: %s payload.bin [output_dir]' % sys.argv[0])
-    sys.exit()
+    signal.signal(signal.SIGINT, signal_handler)
 
-  try:
-    output_dir = sys.argv[2]
-  except IndexError:
-    output_dir = os.getcwd()
+    parser = argparse.ArgumentParser(
+        description="Extract Android OTA payload.",
+        usage="extract_android_ota_payload.py <payload.bin> [target_dir] [--use-local-tools]\n"
+              "  <payload.bin>       : file extracted from the OTA zip file or the OTA zip file\n"
+              "  [target_dir]        : output directory for the extracted file (default: './output')\n"
+              "  [--use-local-tools] : use local xzcat and bzcat if available"
+    )
+    parser.add_argument("filename", type=Path, help=argparse.SUPPRESS)
+    parser.add_argument("output_dir", type=Path, nargs='?', default=Path('output'), help=argparse.SUPPRESS)
+    parser.add_argument("--use-local-tools", action="store_true", help=argparse.SUPPRESS)
+    args = parser.parse_args()
 
-  if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+    args.output_dir.mkdir(parents=True, exist_ok=True)
 
-  main(filename, output_dir)
+    try:
+        if args.use_local_tools:
+            check_local_tools()
+        extract_android_ota_payload(args.filename, args.output_dir, args.use_local_tools)
+    except PayloadError as e:
+        print(e)
+        sys.exit(1)
